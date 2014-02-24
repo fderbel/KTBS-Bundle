385d5b94c7d1ae4823cd49b4db485231eaf67bbd
fatma <fatma@fatma-VirtualBox.(none)>
2014-02-24 15:05:28 +0100

    commit

--- CoatKtbsBundle.php
+++ CoatKtbsBundle.php
@@ -3,7 +3,15 @@
 namespace Coat\KtbsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+use Claroline\CoreBundle\Library\PluginBundle;
 
-class CoatKtbsBundle extends Bundle
+class CoatKtbsBundle extends PluginBundle
 {
+ public function getConfiguration($environment)
+    {
+        $config = new ConfigurationBuilder();
+
+        return $config->addRoutingResource(__DIR__ . '/Resources/config/routing.yml', null, 'coat_ktbs');
+    }
 }
