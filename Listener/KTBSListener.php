d54b2754d9f57edbdb8019e1c1df71e8a2cb7ab9
fatma <fatma@fatma-VirtualBox.(none)>
2014-08-29 17:00:20 +0200

    Without Cache

--- Listener/KTBSListener.php
+++ Listener/KTBSListener.php
@@ -11,10 +11,8 @@ use Claroline\CoreBundle\Event\Log\LogWorkspaceRoleDeleteEvent;
 use Claroline\CoreBundle\Event\Log\LogNotRepeatableInterface;
 use Claroline\CoreBundle\Entity\Log\Log;
 use Claroline\CoreBundle\Event\LogCreateEvent;
-use Claroline\CoreBundle\Manager\RoleManager;
-use Symfony\Component\Security\Core\SecurityContextInterface;
-use Claroline\CoreBundle\Persistence\ObjectManager;
 use JMS\DiExtraBundle\Annotation as DI;
+
 // for ktbs
 //use Claroline\CoreBundle\Event\LogCreateEvent;
 use Claroline\CoreBundle\Event\Log\LogRoleSubscribeEvent;
@@ -22,6 +20,7 @@ use Claroline\CoreBundle\Event\Log\LogUserLoginEvent;
 use Claroline\CoreBundle\Event\Log\LogWorkspaceToolReadEvent;
 use Claroline\CoreBundle\Event\Log\LogUserCreateEvent;
 use Coat\Ktbs\KtbsConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 
 /**
@@ -29,43 +28,47 @@ use Coat\Ktbs\KtbsConfig;
  */
  
  
-class KTBSListener
+class KTBSListener 
 
-{   /**
+{  
+    
+    /**
+     * 
      * @DI\InjectParams({
      *     
      * })
+     * 
      */
-    public function __construct()
-    {
+     
+    public function __construct( ){
         
-    }
-
-    public function createLog(LogCreateEvent $event){   
+      }
+    
+    public function createLog(LogCreateEvent $event){  
+    
         $log = $event->getLog();
         // get user  
         if ($log->getDoer() === null)
             {$user=$log->getReceiver();}
         else 
             { $user=$log->getDoer();}
-        // case with action    
+        // case with action 
+       
         switch ($log->getAction()){
             case LogWorkspaceToolReadEvent::ACTION :
             {           //enter in the workspace and commucation with collector client
                        
                         if (($log->getWorkspace() !== null) && ($user !== null))
                         {
-                            $ktbs = new KtbsConfig($user,$log->getWorkspace()) ;
-                            if ($ktbs->exist)
-                            {
-                              $DataObsel= $ktbs->DataObsel($user,$log->getWorkspace());
-                              $trace_Name = $DataObsel["TraceName"];
-                              $Base_URI = $DataObsel["BaseURI"];
-                              $Model_URI= $DataObsel["modelURI"] ;
+                              $ktbs = new KtbsConfig($user,$log->getWorkspace()) ;
+                              $ktbs->createTrace();
+                              $trace_Name   = $ktbs->getTraceName();
+                              $Base_URI     = $ktbs->getBaseURI();
+                              $Model_URI    = $ktbs->getModelURI();
                               setcookie("TraceName",$trace_Name);
                               setcookie("BAseURI",$Base_URI);
                               setcookie("Model_URI",$Model_URI);
-                             }
+                             
                         }
              }
              default :
